a990abb2efefec900f833a9c54c068a175805921
ryo <ryo@AsaminoMacBook.local>
2017-04-12 10:47:57 +0900

    now use minimist

--- src/realtime.js
+++ src/realtime.js
@@ -1,36 +1,95 @@
 'use strict';
 
+const minimist = require('minimist');
 const rec = require('node-record-lpcm16');
 const speech = require('@google-cloud/speech');
 
-const projectId = 'symbolic-envoy-163901';
-const speechClient = speech({
-  projectId: projectId
-});
+const options = {
+  default: {
+    'sample-rate': 16000,
+    'language-code': 'en-US',   // list: https://cloud.google.com/speech/docs/languages
+  },
+  alias: {
+    f: 'file',          // output file for transcription
+    k: 'key-file',       // google cloud api key file
+    p: 'project-name',  // google cloud project name for speech api
+    l: 'language-code',
+    s: 'sample-rate',
+  }
+};
 
-const languageCode = 'ja-JP';
-const sampleRate = 44100;
+const argv = minimist(process.argv.slice(2), options);
+console.dir(argv);
+
+let speechClient = speech({
+  projectId: argv.p,
+  keyFile: argv.k
+});
 
 let request = {
   config: {
     encoding: 'LINEAR16',
-    sampleRate: sampleRate,
-    languageCode: languageCode
+    sampleRate: argv.s,
+    languageCode: argv.l
   },
   singleUtterance: false,
   interimResults: false,
-  timeout: 10   // somehow this option is not working
+  timeout: 300
 };
 
-rec.start({
-  sampleRate : sampleRate,
-  verbose : false
-}).pipe(speechClient.createRecognizeStream(request))
+let outStream;
+if (argv.f) {
+  const fs = require('fs');
+  outStream = fs.createWriteStream(argv.f);
+}
+
+let recognizeStream = speechClient.createRecognizeStream(request);
+recognizeStream
   .on('error', console.error)
-  .on('data', function(data) {
-    if (data.results && data.results.length > 0) {
-      console.log(data.results);
+  .on('data', (data) => {
+    console.log(data);
+    if (data.error) {
+      console.error('Error happened.');
+      process.exit(1);
+    } else if (data.results && data.results.length > 0) {
+      console.log('[Transcription] ' + data.results);
+      if (argv.f) {
+        outStream.write(data.results + '\n');
+      }
     }
   });
+  // .on('end', () => {
+  //   console.log('All response returned.');
+  //   if (argv.f) {
+  //     outStream.end();
+  //   }
+  // });
 
-console.log('Ctrl-C(SIGINT) to stop.');
+process.on('exit', (code) => {
+  if (argv.f) {
+    outStream.end();
+  }
+  console.log(`Process stopped.`);
+});
+
+rec.start({
+  sampleRate : argv.s,
+  verbose : false
+}).pipe(recognizeStream);
+
+// let firstSIGINT = true;
+// process.on('SIGINT', () => {
+//   if (firstSIGINT) {
+//     rec.stop();
+//     console.log('Recording stopped. Ctrl-C again to force stop.');
+//     firstSIGINT = false;
+//   } else {
+//     recognizeStream.end();
+//     if (argv.f) {
+//       outStream.end();
+//       console.log(process._getActiveHandles().indexOf(outStream));
+//     }
+//     console.log('Killed.');
+//   }
+// });
+// console.log('Ctrl-C(SIGINT) to stop.');
